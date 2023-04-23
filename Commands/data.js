a7e6563964e89da7770a523e2e358a32e31a8cbe
ismager7878 <ao.barbre@gmail.com>
2023-04-23 23:55:06 +0200

    help embed

--- Commands/data.js
+++ Commands/data.js
@@ -1,13 +1,24 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandBuilder, EmbedBuilder } from '@discordjs/builders';
 import { Database } from '../database.js';
+import { command_table } from '../Tools/command_table.js';
 
 export const help = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('How to use the bot'),
     execute: async (message, bottools) => {
-        message.reply('help');
+        const embed = new EmbedBuilder()
+            .setTitle('What am i?')
+            .setAuthor({ name: 'Dungeon Music Master', iconURL: 'https://i.imgur.com/g8lAO6t.png'})
+            .setDescription('Hi, im a bot that plays music, like all the other bots. BUT i also got special features, to make running an online D&D session a breze. Under here i will list all the commands you can use.')
+            .addFields(
+                command_table.map((e) => {
+                    return {name: `/${e.data.name}`, value: e.data.description, inline: false}
+                })
+            )
+        message.reply({ embeds: [embed] });
     }}
+        
 
 export const check = {
     data: new SlashCommandBuilder()
--- Tools/command_table.js
+++ Tools/command_table.js
@@ -1,6 +1,6 @@
 import {pause, play, unpause, stop, ping} from '../commands/basic_controlls.js'
 import {add, queue, skip, clear, shuffle} from '../commands/playlist.js';
-import {check} from '../commands/data.js';
+import {check, help} from '../commands/data.js';
 export const command_table = [
     check,
     pause,
@@ -12,5 +12,5 @@ export const command_table = [
     skip,
     clear,
     shuffle,
-    ping,
+    help,
 ]
