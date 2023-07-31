c1a6288b4589d383e55f8a1136079f2fe6fe5831
Andreas Barbre <ao.barbre@gmail.com>
2023-07-31 16:05:19 +0200

    Done

--- filerenamer.js
+++ filerenamer.js
@@ -1,8 +1,8 @@
 import { newfetchSpotifyTrack } from "./Tools/spotify_handler.cjs";
 import fs from 'fs'
 
-const playlist_url = 'https://open.spotify.com/playlist/2UpxGcCMsNmRPObM9mha44?si=cccbeac120764b19'
-const input_dir= "C:/Users/aobar/Downloads/Test"
+const playlist_url = 'https://open.spotify.com/playlist/2IXsqUDxPtwMBnAeA5azy6?si=9edac75eede846d7'
+const input_dir= "D:/Barbre og Haun på tur"
 
 let songs = []
 let renamed_paths = []
@@ -16,22 +16,25 @@ fs.readdirSync(input_dir).forEach(file=>{
 
 const trackInfoList = await newfetchSpotifyTrack(playlist_url)
 
+console.log(songs)
+
 const isSong = (song, test) => {
     let matches = []
     test.split(' ').forEach(word => {
-        if(!song.toLowerCase().includes(word.toLowerCase().replace('æ', '').replace('å', '').replace('ø', '').replace('.', '').replace("´",''))){
+if(song.toLowerCase().includes(` ${word.toLowerCase().replace('æ', '').replace('å', '').replace('ø', '').replace('.', '').replace("´",'')}`) || song.toLowerCase().includes(`${word.toLowerCase().replace('æ', '').replace('å', '').replace('ø', '').replace('.', '').replace("´",'')} `)){
+            matches.push(true)
+        }else{
             matches.push(false)
             return
-        }else{
-            matches.push(true)
         }
     })
-    return matches.filter(x=>x==false).length/matches.length <= 0.5 ? true : false;
+    return matches.filter(x=>x==false).length/matches.length <= 0.3 ? true : false;
 }
 songs.forEach(song => {
  const song_formatted = trackInfoList.filter(x=>{
     return isSong(song ,x.title)
  })[0]
+ console.log(song)
  console.log(song_formatted)
  renamed_paths.push(`${input_dir}/${song_formatted.title} - ${song_formatted.subtitle}.mp3`)
 })
@@ -40,5 +43,9 @@ console.log(old_paths)
 console.log(renamed_paths)
 
 for(let i = 0; i < old_paths.length; i++){
-    fs.renameSync(old_paths[i], renamed_paths[i])
+    try{
+        fs.renameSync(old_paths[i], renamed_paths[i])
+    } catch (e){
+        console.log(old_paths[i] + e)
+    }
 }
